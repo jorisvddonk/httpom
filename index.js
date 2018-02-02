a557d12b3bfab05acd81fdf7a71084eb11c1e0cc
Joris van de Donk <jorisvddonk@gmail.com>
2018-02-02 12:00:04 +0100

    Basic commandline functionality

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+#!/usr/bin/env node
+
+var program = require('commander');
 var parsePomfile = require('./lib/parsePomfile');
 var prepareRequest = require('./lib/prepareRequest');
 var fetch = require('node-fetch');
 var fs = require('fs');
+var package = require('./package.json');
 
-var parsed_pomfile = parsePomfile(fs.readFileSync('./examples/4.pom'));
-var prepared_request = prepareRequest(parsed_pomfile);
-
-fetch(prepared_request.url, prepared_request.options).then(function(res){
-  console.log(res.body.read().toString());
+program.version(package.version)
+.option('--web', 'Use web-based interface')
+.option('--cli', 'Use command-line interface')
+.command('*')
+.action(function(pomfile){
+  var parsed_pomfile = parsePomfile(fs.readFileSync(pomfile));
+  var prepared_request = prepareRequest(parsed_pomfile);
+  
+  fetch(prepared_request.url, prepared_request.options).then(function(res){
+    console.log(res.body.read().toString());
+  });
+  
 });
+program.parse(process.argv);
